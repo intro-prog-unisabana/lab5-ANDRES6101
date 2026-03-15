66882acb4893f6243c40f075f8a497ee24828d85
Sergio Andres Tiria Lancheros <sergiotila@unisabana.edu.co>
2026-03-15 15:16:40 -0500

    V3

--- functions.py
+++ functions.py
@@ -1,13 +1,4 @@
-def promedio_estudiante(calificaciones):
+def obtener_precio_usuario():
+    precio = input("Enter the item's price:\n")
+    return float(precio)
 
-    if len(calificaciones) == 0:
-        return 0.0
-    
-    promedio = sum(calificaciones) / len(calificaciones)
-    return float(promedio)
-
-
-# Llamar la función
-notas = [50,10,3.6,40,2.5]
-resultado = promedio_estudiante(notas)
-print(resultado)
---
+++ scope.py
@@ -0,0 +1,22 @@
+int_value = None
+str_value = None
+
+#Almacena un entero global y una cadena de texto global.
+
+def set_globals(some_int, some_str):
+    global int_value
+    global str_value
+
+    int_value = some_int
+    str_value = some_str
+
+#  valores guardados en las variables globales.
+
+def get_globals():
+    return (int_value, str_value)
+
+
+#uso
+print(get_globals())      
+set_globals(10, "Hello")
+print(get_globals())  
