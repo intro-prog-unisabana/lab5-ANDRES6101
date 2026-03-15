068a9ad1100053a9c1ef641e6cffe79085eb4263
Sergio Andres Tiria Lancheros <sergiotila@unisabana.edu.co>
2026-03-15 17:41:04 -0500

    V6

--- scope.py
+++ scope.py
@@ -1,22 +1,10 @@
-int_value = 0
-str_value = 0
-
-#Almacena un entero global y una cadena de texto global.
+int_value = None
+str_value = None
 
 def set_globals(some_int, some_str):
     global int_value
     global str_value
-
     int_value = some_int
     str_value = some_str
-
-#  valores guardados en las variables globales.
-
 def get_globals():
     return (int_value, str_value)
-
-
-#uso
-print(get_globals())      
-set_globals(10, "Hello")
-print(get_globals())  
