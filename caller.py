068eaf78d275a8a9e6a49dd607a7b936709888dc
Sergio Andres Tiria Lancheros <sergiotila@unisabana.edu.co>
2026-03-15 21:20:23 -0500

    V11

--- caller.py
+++ caller.py
@@ -1,15 +1,9 @@
-
-
+from mystery_module import transform_data
 # FREEZE CODE BEGIN
 x = int(input())
 y = float(input())
 # FREEZE CODE END
-
-
-from mystery_module import transform_data
-
-x = int(input())
-y = float(input())
 result = transform_data(x, y, "quiz_test")
+
 print(result)
 
